e507f4820efd795f14ed4d1b571e2404788e18e9
Kalpesh Bavaliya <kalpeshbavaliya2005@gmail.com>
2025-09-19 18:33:03 +0530

    semester and varchar datatype change

---
+++ database/migrations/2025_09_19_100214_change_marks_obtain_to_varchar_in_student_result.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('student_results', function (Blueprint $table) {
+            $table->string('total_cce_obt', 20)->nullable()->default(null)->change();
+            $table->string('total_see_obt', 20)->nullable()->default(null)->change();
+            $table->string('total_marks_obt', 20)->nullable()->default(null)->change();
+        });
+
+        Schema::table('student_subject_results', function (Blueprint $table) {
+            $table->string('cce_obtained', 20)->nullable()->default(null)->change();
+            $table->string('see_obtained', 20)->nullable()->default(null)->change();
+            $table->string('total_obtained', 20)->nullable()->default(null)->change();
+        });
+
+        Schema::table('students', function (Blueprint $table) {
+            $table->string('studentClass')->nullable()->after('semesterId');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('student_results', function (Blueprint $table) {
+            $table->integer('total_cce_obt')->change();
+            $table->integer('total_see_obt')->change();
+            $table->integer('total_marks_obt')->change();
+        });
+        Schema::table('student_subject_results', function (Blueprint $table) {
+            $table->integer('cce_obtained')->change();
+            $table->integer('see_obtained')->change();
+            $table->integer('total_obtained')->change();
+        });
+
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropColumn('studentClass');
+        });
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -16,30 +16,41 @@ Route::post('loginadmin', [AuthController::class, 'login']);
 
 // Protected routes
 Route::middleware(['jwt.auth'])->group(function () {
+});
     Route::get('me', [AuthController::class, 'me']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
-
-    Route::post('college', [CollegeController::class, 'handle']);
-    Route::post('semester', [SemesterController::class, 'handle']);
-    Route::post('student', [StudentController::class, 'handle']);
-    Route::post('student/excel', [StudentController::class, 'uploadExcel']);
-    Route::post('examtype', [ExamTypeController::class, 'handle']);
-    Route::get('examtype/dropdown', [ExamTypeController::class, 'dropdown']);
+    
+    
+    // =============== CRUE API WITH ACTION =======================
     Route::post('user', [UserController::class, 'handle']);
     Route::post('role', [RoleController::class, 'handle']);
+    Route::post('college', [CollegeController::class, 'handle']);
+    Route::post('student', [StudentController::class, 'handle']);
+    Route::post('semester', [SemesterController::class, 'handle']);
+    Route::post('examtype', [ExamTypeController::class, 'handle']);
+    Route::post('result', [StudentResultController::class, 'handle']);
+    Route::post('result/subject', [StudentSubjectResultController::class, 'handle']);
+    
+    
+    // =============== EXCEL SHEET API =======================
     Route::post('result/excel', [StudentResultController::class, 'importResultsExcel']);
+    Route::post('student/excel', [StudentController::class, 'uploadExcel']);
     Route::post('result/excel/internal', [StudentResultController::class, 'importResultsInternal']);
+    
+    // =============== NODEJS RELATED API =======================
     Route::post('resulttonode', [StudentResultController::class, 'sendResultsToNode']);
     Route::post('result/subject/autocreate', [StudentResultController::class, 'updateResultWithSubjects']);
     
-    Route::get('result/subject', function () {
+    // =============== RESULT VIEW ALL API =======================
+     Route::post('result/viewresult', [StudentSubjectResultController::class, 'getStudentResult']);
+     Route::get('examtype/dropdown', [ExamTypeController::class, 'dropdown']);
+     Route::get('semester/dropdown', [SemesterController::class, 'dropdown']);
+    
+    
+    Route::get('hello', function () {
         return response()->json([
             'status' => true,
             'message' => 'Hello! Route is working.'
         ]);
     });
-});
-
-Route::post('result/subject', [StudentSubjectResultController::class, 'handle']);
-Route::post('result', [StudentResultController::class, 'handle']);
